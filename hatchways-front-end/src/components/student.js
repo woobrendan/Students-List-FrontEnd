6f885b229cdd085055c0113e2985b6c5c7ac00e4
Brendan Woo <woo.brendan@gmail.com>
2022-04-26 13:47:48 -0400

    map over array of students to output info

--- hatchways-front-end/src/components/student.js
+++ hatchways-front-end/src/components/student.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function StudentInfo() {
   const [students, setStudents] = useState([])
-  console.log("students: ", students)
+  
   useEffect(() => {
     axios.get(`https://api.hatchways.io/assessment/students`)
       .then((res) => {
@@ -12,9 +12,23 @@ export default function StudentInfo() {
       .catch((err) => console.log("Error: ", err))
   }, []);
 
+  const mappedStudents = students.map(student => (
+    <div className="student-info-container">
+      <div className="student--img">
+        <img src={student.pic} alt={student.firstName}/>
+      </div>
+      <h2>{student.firstName} {student.lastName}</h2>
+      <p>Email: {student.email}</p>
+      <p>Company: {student.company}</p>
+      <p>Skill: {student.skill}</p>
+      <p>Average: {student.average}</p>
+
+    </div>
+  ))
+
   return (
     <>
-    Hello
+      {mappedStudents}
     </>
   )
 }
