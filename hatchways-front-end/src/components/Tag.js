09fa97b9f7560b71adf17f5c2b3c188376f2f945
Brendan Woo <woo.brendan@gmail.com>
2022-05-03 16:09:55 -0400

    set up style for textfield, add usestate to manage value of textfield and add onchange handler

--- hatchways-front-end/src/components/Tag.js
+++ hatchways-front-end/src/components/Tag.js
@@ -1,6 +1,21 @@
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+
 export default function Tag() {
+  const [tag, setTag] = useState("");
+  const handleChange = (event) => {
+    setTag(event.target.value)
+  }
   return (
     <>
+    <TextField 
+      id="standard-basic" 
+      variant="standard"
+      label="Add a tag"
+      size="small"
+      value={tag}
+      onChange={handleChange}
+    />
     </>
   )
 }
