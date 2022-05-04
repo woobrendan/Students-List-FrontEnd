0026b90ee5eb1c9af23a852a266bf72e18aed449
Brendan Woo <woo.brendan@gmail.com>
2022-05-04 16:35:28 -0400

    add key to tag list  tags

--- hatchways-front-end/src/components/TagList.js
+++ hatchways-front-end/src/components/TagList.js
@@ -2,7 +2,7 @@ import { Chip } from '@mui/material';
 
 export default function TagList(props) {
   const mappedTags = props.tagList.map((tag, index) => (
-    <Chip label={tag} key={index + 1}/>
+    <Chip label={tag} key={index}/>
   ))
   return (
     <>
