4c690f52ccc425411348860e34b4fc70dc16f47a
Brendan Woo <woo.brendan@gmail.com>
2022-05-04 16:37:28 -0400

    add comments

--- hatchways-front-end/src/components/TagList.js
+++ hatchways-front-end/src/components/TagList.js
@@ -1,5 +1,6 @@
 import { Chip } from '@mui/material';
 
+//receive  tag list from Tag component, and render out chip components accordingly
 export default function TagList(props) {
   const mappedTags = props.tagList.map((tag, index) => (
     <Chip label={tag} key={index}/>
