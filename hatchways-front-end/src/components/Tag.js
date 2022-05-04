87c156c62d13ca9520ae2ab69dd54d5c8c018572
Brendan Woo <woo.brendan@gmail.com>
2022-05-04 16:16:58 -0400

    setup handlesubmit and taglist

--- hatchways-front-end/src/components/Tag.js
+++ hatchways-front-end/src/components/Tag.js
@@ -1,21 +1,36 @@
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import TagList from './TagList';
+import { Chip } from '@mui/material';
 
 export default function Tag() {
   const [tag, setTag] = useState("");
+  const [tagList, setTagList] = useState([])
   const handleChange = (event) => {
     setTag(event.target.value)
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTagList((prev) => ([
+      ...prev, tag
+    ]))
+    setTag("")
+  }
+
   return (
     <>
-    <TextField 
-      id="standard-basic" 
-      variant="standard"
-      label="Add a tag"
-      size="small"
-      value={tag}
-      onChange={handleChange}
-    />
+      <TagList tagList={tagList} />
+      <form onSubmit={handleSubmit}>
+        <TextField 
+          id="standard-basic" 
+          variant="standard"
+          label="Add a tag"
+          size="small"
+          value={tag}
+          onChange={handleChange}
+        />
+      </form>
     </>
   )
 }
